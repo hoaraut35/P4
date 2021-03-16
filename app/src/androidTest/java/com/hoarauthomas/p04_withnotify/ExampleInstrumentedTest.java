a5e8ac6fac9ebf3c67be40ce74e2e4f0821703f5
HOARAU Thomas <perso.thomas.hoarau@gmail.com>
2021-03-16 13:21:50 +0100

    travail sur test

--- app/src/androidTest/java/com/hoarauthomas/p04_withnotify/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/hoarauthomas/p04_withnotify/ExampleInstrumentedTest.java
@@ -1,13 +1,23 @@
 package com.hoarauthomas.p04_withnotify;
 
+import android.app.Activity;
 import android.content.Context;
 
+import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
 
+import com.hoarauthomas.p04_withnotify.model.Meeting;
+
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.hasMinimumChildCount;
 import static org.junit.Assert.*;
 
 /**
@@ -17,6 +27,21 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    private MainActivity mActivity;
+
+
+    @Rule
+    public ActivityTestRule<MainActivity> meetingActivityRule = new ActivityTestRule(MainActivity.class);
+
+    @Before
+    public void setup()
+    {
+        mActivity = meetingActivityRule.getActivity();
+        //assertThat(mActivity, notNullValue);
+    }
+
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
@@ -26,10 +51,12 @@ public class ExampleInstrumentedTest {
 
 
 
+    //This test check if we have a minimum of one item in the recyclerview
     @Test
     public void myMeetingList_shouldNotBeEmpty()
     {
-       // o,View
+        onView(ViewMatchers.withId(R.id.recyclerview)).check(matches(hasMinimumChildCount(1)));
+
     }
 
 
