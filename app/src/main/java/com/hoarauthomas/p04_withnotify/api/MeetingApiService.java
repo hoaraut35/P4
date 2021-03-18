a51f362b831ade55c352082f4a825c421cc5a47f
HOARAU Thomas <perso.thomas.hoarau@gmail.com>
2021-03-18 14:03:04 +0100

    nettoyage du code

--- app/src/main/java/com/hoarauthomas/p04_withnotify/api/MeetingApiService.java
+++ app/src/main/java/com/hoarauthomas/p04_withnotify/api/MeetingApiService.java
@@ -7,17 +7,14 @@ import com.hoarauthomas.p04_withnotify.model.MeetingRoom;
 import java.util.List;
 
 public interface MeetingApiService {
-
-
-
     List<Collaborator> getCollaborators();
+
     List<Meeting> getMeetings();
+
     List<MeetingRoom> getMeetingsRooms();
 
     void addMeeting(Meeting meeting);
+
     void deleteMeeting(Meeting meeting);
-
-
-
 
 }
