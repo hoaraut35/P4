065c9beb64a8ab47c048ff68a5b0992dc480e397
HOARAU Thomas <perso.thomas.hoarau@gmail.com>
2021-03-18 13:42:15 +0100

    modif adapter for emptylist

--- app/src/main/java/com/hoarauthomas/p04_withnotify/adapter/MeetingAdapter.java
+++ app/src/main/java/com/hoarauthomas/p04_withnotify/adapter/MeetingAdapter.java
@@ -28,8 +28,7 @@ import com.hoarauthomas.p04_withnotify.view.AddMeetingActivity;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MeetingAdapter extends RecyclerView.Adapter<MeetingAdapter.WordViewHolder> implements AddMeetingActivity.Listener
-{
+public class MeetingAdapter extends RecyclerView.Adapter<MeetingAdapter.WordViewHolder> implements AddMeetingActivity.Listener {
     private List<Meeting> mListMeeting = null;
     private LayoutInflater mInflater;
     List<Meeting> list;
@@ -39,20 +38,20 @@ public class MeetingAdapter extends RecyclerView.Adapter<MeetingAdapter.WordView
 
     //----------------------------------------------------------------------------------------------
     private Listener callback;
-    public interface Listener
-    {
+
+    public interface Listener {
         void onClickDelete(int position, Meeting meetingToDel);
+
         void onUpdateList(List<Meeting> meets);
 
     }
     //----------------------------------------------------------------------------------------------
 
-    public MeetingAdapter(Context context, List<Meeting> mListMeeting2)
-    {
-        Log.i("THOMAS","Constructeur adapteur : " + mListMeeting2.size());
+    public MeetingAdapter(Context context, List<Meeting> mListMeeting2) {
+        Log.i("THOMAS", "Constructeur adapteur : " + mListMeeting2.size());
         mInflater = LayoutInflater.from(context);
         this.mListMeeting = mListMeeting2;
-      //  this.mListMeeting = service.getMeetings();
+        //  this.mListMeeting = service.getMeetings();
         this.mCopyListMeeting = new ArrayList<Meeting>(mListMeeting2);
     }
 
@@ -60,58 +59,53 @@ public class MeetingAdapter extends RecyclerView.Adapter<MeetingAdapter.WordView
     //**********************************************************************************************
 
 
-
     //**********************************************************************************************
 
     @NonNull
     @Override
-    public MeetingAdapter.WordViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
-    {
+    public MeetingAdapter.WordViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View mItemView = mInflater.inflate(R.layout.custom_meeting_layout, parent, false);
         return new WordViewHolder(mItemView, this);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MeetingAdapter.WordViewHolder holder, int position)
-    {
+    public void onBindViewHolder(@NonNull MeetingAdapter.WordViewHolder holder, int position) {
         Meeting mCurrent = mListMeeting.get(position);
-        holder.mAvatar.getBackground().setTint(ContextCompat.getColor(holder.itemView.getContext(),R.color.col5));
 
-        switch (mCurrent.getmPosition().toString())
-        {
+        switch (mCurrent.getmPosition()) {
             case "Peach":
-                holder.mAvatar.getBackground().setTint(ContextCompat.getColor(holder.itemView.getContext(),R.color.col1));
+                holder.mAvatar.getBackground().setTint(ContextCompat.getColor(holder.itemView.getContext(), R.color.col1));
                 break;
             case "Toad":
-                holder.mAvatar.getBackground().setTint(ContextCompat.getColor(holder.itemView.getContext(),R.color.col2));
+                holder.mAvatar.getBackground().setTint(ContextCompat.getColor(holder.itemView.getContext(), R.color.col2));
                 break;
             case "Yoshi":
-                holder.mAvatar.getBackground().setTint(ContextCompat.getColor(holder.itemView.getContext(),R.color.col3));
+                holder.mAvatar.getBackground().setTint(ContextCompat.getColor(holder.itemView.getContext(), R.color.col3));
                 break;
             case "Bowser":
-                holder.mAvatar.getBackground().setTint(ContextCompat.getColor(holder.itemView.getContext(),R.color.col4));
+                holder.mAvatar.getBackground().setTint(ContextCompat.getColor(holder.itemView.getContext(), R.color.col4));
                 break;
             case "Wario":
-                holder.mAvatar.getBackground().setTint(ContextCompat.getColor(holder.itemView.getContext(),R.color.col5));
+                holder.mAvatar.getBackground().setTint(ContextCompat.getColor(holder.itemView.getContext(), R.color.col5));
                 break;
             case "Waluigi":
-                holder.mAvatar.getBackground().setTint(ContextCompat.getColor(holder.itemView.getContext(),R.color.col6));
+                holder.mAvatar.getBackground().setTint(ContextCompat.getColor(holder.itemView.getContext(), R.color.col6));
                 break;
             case "Bidosaure":
-                holder.mAvatar.getBackground().setTint(ContextCompat.getColor(holder.itemView.getContext(),R.color.col7));
+                holder.mAvatar.getBackground().setTint(ContextCompat.getColor(holder.itemView.getContext(), R.color.col7));
                 break;
             case "Bero":
-                holder.mAvatar.getBackground().setTint(ContextCompat.getColor(holder.itemView.getContext(),R.color.col8));
+                holder.mAvatar.getBackground().setTint(ContextCompat.getColor(holder.itemView.getContext(), R.color.col8));
                 break;
             case "Mario":
-                Log.i("COLOR","couleur col9");
-                holder.mAvatar.getBackground().setTint(ContextCompat.getColor(holder.itemView.getContext(),R.color.col9));
+                Log.i("COLOR", "couleur col9");
+                holder.mAvatar.getBackground().setTint(ContextCompat.getColor(holder.itemView.getContext(), R.color.col9));
                 break;
             case "Xmen":
-                holder.mAvatar.getBackground().setTint(ContextCompat.getColor(holder.itemView.getContext(),R.color.col10));
+                holder.mAvatar.getBackground().setTint(ContextCompat.getColor(holder.itemView.getContext(), R.color.col10));
                 break;
             default:
-                holder.mAvatar.getBackground().setTint(ContextCompat.getColor(holder.itemView.getContext(),R.color.col1));
+                holder.mAvatar.getBackground().setTint(ContextCompat.getColor(holder.itemView.getContext(), R.color.col1));
 
         }
 
@@ -123,7 +117,7 @@ public class MeetingAdapter extends RecyclerView.Adapter<MeetingAdapter.WordView
             @Override
             public void onClick(View v) {
                 mListMeeting.remove(position);
-               // service.getMeetings().remove(position);
+                // service.getMeetings().remove(position);
                 notifyDataSetChanged();
             }
         });
@@ -137,14 +131,12 @@ public class MeetingAdapter extends RecyclerView.Adapter<MeetingAdapter.WordView
     }
 
     @Override
-    public void onAddMeeting(int position)
-    {
-        Log.i("THOMAS","callback add");
+    public void onAddMeeting(int position) {
+        Log.i("THOMAS", "callback add");
     }
 
 
-    class WordViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener
-    {
+    class WordViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         public final TextView wordItemView;
         final MeetingAdapter mAdapter;
         public ImageView mAvatar;
@@ -154,16 +146,15 @@ public class MeetingAdapter extends RecyclerView.Adapter<MeetingAdapter.WordView
         public TextView mParticipants;
         public ImageView mDeleteImage;
 
-        public WordViewHolder(@NonNull View itemView, MeetingAdapter adapter)
-        {
+        public WordViewHolder(@NonNull View itemView, MeetingAdapter adapter) {
             super(itemView);
             wordItemView = itemView.findViewById(R.id.view_name_meeting);
             mNameView = itemView.findViewById(R.id.view_name_meeting);
             mStartTime = itemView.findViewById(R.id.view_start_time_meeting);
-            mAvatar =itemView.findViewById(R.id.view_image_meeting);
-            mPlace =itemView.findViewById(R.id.view_position_meeting);
+            mAvatar = itemView.findViewById(R.id.view_image_meeting);
+            mPlace = itemView.findViewById(R.id.view_position_meeting);
             mParticipants = itemView.findViewById(R.id.view_email_meeting);
-            mDeleteImage =itemView.findViewById(R.id.view_delete_meeting);
+            mDeleteImage = itemView.findViewById(R.id.view_delete_meeting);
             this.mAdapter = adapter;
             itemView.setOnClickListener(this);
         }
@@ -173,11 +164,11 @@ public class MeetingAdapter extends RecyclerView.Adapter<MeetingAdapter.WordView
 
             int mPosition = getLayoutPosition();
 
-           // Meeting element = mListMeetingtemp.get(mPosition);
+            // Meeting element = mListMeetingtemp.get(mPosition);
 
 
             //mListMeeting.addAll(mPosition,)
-           // mListMeetings.set(mPosition, "Clicked! " + element);
+            // mListMeetings.set(mPosition, "Clicked! " + element);
             mAdapter.notifyDataSetChanged();
         }
     }
